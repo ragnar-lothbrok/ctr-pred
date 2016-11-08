7cda0f0612245f12f7e47162c5fca77b021e3834
raghunandan <raghunandan.gupta@exclusively.com>
2016-11-08 20:02:59 +0530

    xgboost related code added.

--- com/company/samplelibsvmtest.py
+++ com/company/samplelibsvmtest.py
@@ -4,20 +4,39 @@ import pandas as pd
 from sklearn.datasets import dump_svmlight_file
 import sklearn.datasets
 
-#This will load libsvm file
-intialData = sklearn.datasets.load_svmlight_file('/home/raghunandangupta/Downloads/soc_gen_data/cde.txt')
+# This will load libsvm file
+data, outcome = sklearn.datasets.load_svmlight_file('/home/raghunandangupta/Downloads/soc_gen_data/cde.txt', multilabel=True, zero_based=False)
 
-#This will create data frame
-df = pd.SparseDataFrame([ pd.SparseSeries(intialData[0][i].toarray().ravel()) 
-                              for i in np.arange(2) ])
+columnCount = data.shape[1]
 
-#Here we are selecting column which will participate in prediction
-X = df.loc[:, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 24]]
-y = df[0]
+df2 = pd.DataFrame(outcome)
+df2[columnCount] = df2[0]
+print df2.columns
 
-#Converting again in matrix 
+
+# This will create data frame
+df = pd.SparseDataFrame([ pd.SparseSeries(data[i].toarray().ravel()) 
+                              for i in np.arange(outcome.__len__()) ])
+
+# Here we are selecting column which will participate in prediction
+X = df.loc[:, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]]
+y = df2[0]
+
+frames = [df, df2[columnCount]]
+
+df = pd.concat(frames,axis=1)
+
+df[columnCount] = df[0]
+df[0] = df2[0]
+
+print df.columns
+
+# Converting again in matrix 
 dtrain = xgb.DMatrix(df)
 
-#Below lines are when we dump data in file
-# modifiedFileName = '/home/raghunandangupta/Downloads/soc_gen_data/cde1.txt'
-# dump_svmlight_file(X, y, modifiedFileName, zero_based=True, multilabel=False)
+# print dtrain.num_col()
+# print dtrain.num_row()
+
+# Below lines are when we dump data in file
+modifiedFileName = '/home/raghunandangupta/Downloads/soc_gen_data/cde1.txt'
+dump_svmlight_file(X, y, modifiedFileName, zero_based=False, multilabel=False)
